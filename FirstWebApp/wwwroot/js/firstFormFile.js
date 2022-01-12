ba99132ed20344ee15e7df26fd39fcd58c74f4ef
Jerron <jerron.byu@gmail.com>
2022-01-12 14:41:41 -0700

    Add project files.

---
+++ FirstWebApp/wwwroot/js/firstFormFile.js
@@ -0,0 +1,6 @@
+﻿$("#formBtn").click(function () {
+    alert("From " + $("#labeltxt").val() +
+        " input: " + $("#inputtxt").val() +
+        " text box: " + $("#messtxt").val())
+    $("#picture1").fadeToggle("slow")
+})
